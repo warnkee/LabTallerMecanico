7ce144a4ff7dcdb3e00aac3b303508ac3dd2ac5f
saliim <erwarnkeva@est.utn.ac.cr>
2024-08-08 13:55:10 -0600

    Clase vehiculo

--- Vehiculos/Vehiculo.java
+++ Vehiculos/Vehiculo.java
@@ -2,5 +2,42 @@
 package Vehiculos;
 
 public class Vehiculo {
+    private String matricula;
+   private String modelo;
+    private String marca;
+    private int annioFabricacion;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAnnioFabricacion() {
+        return annioFabricacion;
+    }
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String matricula, String modelo, String marca, int annioFabricacion) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.annioFabricacion = annioFabricacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", modelo=" + modelo + ", marca=" + marca + ", annioFabricacion=" + annioFabricacion + '}';
+    }
+    
+    
     
 }
